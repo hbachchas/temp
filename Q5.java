6d5a1ccf8cab18ab6b57c64190e0517ce3c64391
Himanshu Bachchas <5391670+hbachchas@users.noreply.github.com>
2018-11-23 17:43:06 +0530

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,19 @@
+
+// Interface BinaryArray
+  // Abstract method sort2InOn(arr)
+
+// Interface TernaryArray
+  // Abstract method sort3InOn(arr)
+  
+// Class multiInherit(BinaryArray, TernaryArray)
+  // define sort2InOn(arr) and sort3InOn(arr)
+  // define PSVM main()
+    // instantiate the the interface and
+    // sort two arrays by using these methods
+    
+    
+    
+    
+    
+    
+// take alphabet in uppercase from user, then convert it into lower-case
