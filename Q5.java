56de09a4b76d54fa92058dec41650e612cdc74f5
Himanshu Bachchas <5391670+hbachchas@users.noreply.github.com>
2018-11-23 17:55:11 +0530

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -17,3 +17,18 @@
     
     
 // take alphabet in uppercase from user, then convert it into lower-case
+import java.util.Scanner;
+import java.lang.Character;
+
+public class HelloWorld{
+     public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+        // (1)
+        char ch = sc.next().charAt(0);
+        ch = Character.toLowerCase(ch);
+       
+        // (2)
+        String str = sc.next();
+        str.toLowerCase().charAt(0)
+     }
+}
